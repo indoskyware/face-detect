b7ce692a37872b6dd12a72c6936808b627550e37
Topik Hidayat <topikhdyt87@gmail.com>
2023-06-06 13:40:16 +0700

    Feat: Bake Orientation

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const detectFaces = require("./face-detection");
 const logger = require("./logger");
+const sharp = require("sharp");
 
 app.use(express.json());
 app.use(fileUpload());
@@ -40,6 +41,7 @@ app.post("/face-detection", async (req, res) => {
     const pathUpload = __dirname + "/upload/";
 
     let fileName;
+    let ext = ".jpg";
     if (id === undefined) {
       const now = new Date();
       const year = now.getFullYear();
@@ -48,11 +50,30 @@ app.post("/face-detection", async (req, res) => {
       const hour = String(now.getHours()).padStart(2, "0");
       const minute = String(now.getMinutes()).padStart(2, "0");
 
-      fileName = `${year}${month}${day}_${hour}${minute}_${uuidv4()}.jpg`;
+      fileName = `${year}${month}${day}_${hour}${minute}_${uuidv4()}`;
     } else {
-      fileName = `${id}_${uuidv4()}.jpg`;
+      fileName = `${id}_${uuidv4()}`;
     }
-    image.mv(pathUpload + fileName);
+    await image.mv(pathUpload + fileName + ext);
+
+    const metadata = await sharp(pathUpload + fileName + ext).metadata();
+
+    // Check if the image width is greater than the height
+    if (metadata.width > metadata.height) {
+      // Rotate the image counter-clockwise by 90 degrees
+      await sharp(pathUpload + fileName + ext)
+        .rotate()
+        .toFile(pathUpload + fileName + "-rotated" + ext);
+
+      // Delete the original image file
+      fs.unlinkSync(`${pathUpload}${fileName}${ext}`, { force: true });
+
+      // Update the file name to include the "-rotated" suffix
+      fileName = fileName + "-rotated" + ext;
+    } else {
+      // Keep the original file name
+      fileName = fileName + ext;
+    }
 
     // Face Detection Image
     const data = await detectFaces(pathUpload, fileName, minConfidence);
